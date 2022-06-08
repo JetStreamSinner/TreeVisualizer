1b31de3207bc1951766f96e960485e335ba49148
Jetstream <bigalex934@gmail.com>
2022-06-08 12:56:56 +0300

    Remove unnecessary termination

--- source/implementations/c_image/c_image_context_window.cpp
+++ source/implementations/c_image/c_image_context_window.cpp
@@ -1,9 +1,7 @@
 #include "c_image_context_window.h"
 
 CImageContextWindow::CImageContextWindow(int width, int height, const Color &background, const std::string &window_title) : CImageContextBase(width, height, background),
-                                                                                                                _title(window_title) {
-    // TODO: Throw error, when trying to save in .png file.
-    std::terminate();
+                                                                                                                            _title(window_title) {
 }
 
 void CImageContextWindow::update() {
